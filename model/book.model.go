f0b332567b640b5ee4a1bba02328d2928b68945d
anjarmath <anjarmath@gmail.com>
2025-01-17 04:12:32 +0700

    update model

--- model/book.model.go
+++ model/book.model.go
@@ -1,9 +1,18 @@
 package model
 
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
 type Book struct {
-	Judul     string `json:"judul"`
-	Deskripsi string `json:"deskripsi"`
-	Harga     int    `json:"harga"`
+	Id        uuid.UUID  `gorm:"type:uuid;default:uuid_generate_v4();primaryKey" json:"id"`
+	Judul     string     `json:"judul"`
+	Deskripsi string     `json:"deskripsi"`
+	Harga     int        `json:"harga"`
+	CreatedAt time.Time  `gorm:"default:now();" json:"created_at"`
+	UpdatedAt *time.Time `json:"updated_at"`
 }
 
 var BooksCollection []Book
